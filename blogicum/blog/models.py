f0f7e4884dda908e18425fe31e9f575720e57cc1
Egor Okhin <ego0777@gmail.com>
2025-05-13 20:08:05 +0300

    Блять

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+
 
 class Post(models.Model):
-
     title = models.CharField('Название', max_length=256)
     text = models.TextField('Описание')
     pub_date = models.DateTimeField("Дата публикации")
     author = models.ForeignKey()
-    
+
     class Meta:
         verbose_name = _("Пост")
         verbose_name_plural = _("Посты")
@@ -16,4 +17,4 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
+        return reverse("post_detail", kwargs={"post_id": self.pk})
