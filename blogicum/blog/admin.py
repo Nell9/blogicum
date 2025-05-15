84a24603856d051b643f52115da23e43d4e6c0fb
Рабочий пк <gmail.com>
2025-05-15 13:31:02 +0300

    Улучшена админка, дописаны view, готовый проект

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Location, Category, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category',
+        'is_published'
+    )
+    list_editable = (
+        'is_published',
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+
+
 admin.site.register(Location)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,20 +1,35 @@
-from django.http import Http404
-from django.shortcuts import render
+from datetime import datetime
+
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
+from django.utils import timezone
 
 from blog.models import Post, Category, Location
 
 
 def index(request):
-    posts = Post.objects.all()[:5]
+    posts = Post.objects.all().filter(Q(is_published=True) & Q(
+        pub_date__lte=timezone.now()) & Q(category__is_published=True))[:5] 
     return render(request, 'blog/index.html', {'post_list': posts})
 
 
 def category_posts(request, category_slug):
     posts = Post.objects.all().filter(
-        Q(is_published=True) & Q(category__slug=category_slug)
+        Q(is_published=True) & Q(category__slug=category_slug) & Q(
+            pub_date__lte=timezone.now()) & Q(category__is_published=True)
     )
-    return render(request, 'blog/category.html', {'post_list': posts})
+    category = get_object_or_404(Category, slug=category_slug)
+    return render(request,
+                  'blog/category.html',
+                  {'post_list': posts,
+                   'category': category.title}
+                  )
+
 
 def post_detail(request, post_id):
-    pass
+    post = get_object_or_404(Post, id=post_id, pub_date__lte=timezone.now())
+
+    return render(request,
+                  'blog/detail.html',
+                  {'post': post}
+                  )
